4d82140fbd73a6c3d9908f78872af1e8fd9f455f
Aaron Smith <aaron.j.smith2019@gmail.com>
2022-10-11 11:05:57 -0700

    Install dependencies

--- rps-be/website/index.js
+++ rps-be/website/index.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const path = require('path');
 
-const app = express();
-const PORT = 8080;
-const path = path.resolve(__dirname, './build/');
+const serveWebsite = () => {
+  const app = express();
+  const PORT = 8080;
 
-app.use(express.static(path));
+  app.use(express.static(path.resolve(__dirname, './build/')));
 
-app.get('/', function (req,res) {
-  res.sendFile(path.resolve(__dirname, './build/index.html'));
-});
+  app.get('/', function (req,res) {
+    res.sendFile(path.resolve(__dirname, './build/index.html'));
+  });
 
-// Handle requests for a route that does not exist
-app.use(function (req, res, next) {
-  res.status(404).send("Error: We're unable to find the information you are looking for. Please try again.");
-});
+  // Handle requests for a route that does not exist
+  app.use(function (req, res, next) {
+    res.status(404).send("Error: We're unable to find the information you are looking for. Please try again.");
+  });
+
+  app.listen(PORT);
+};
+
+module.exports = {
+  serveWebsite,
+};
